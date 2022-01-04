d2c7616802aa0538b02e859f5491f40a81e60896
Lukcnt <cantoni.lucas@gmail.com>
2022-01-04 00:40:07 -0300

    Program that randomly choose someone to pay the bill, update with another solution

--- Day 4/Exercise 2 - Banker Roulette/Exercise 2 - Banker Roulette.py
+++ Day 4/Exercise 2 - Banker Roulette/Exercise 2 - Banker Roulette.py
@@ -7,3 +7,10 @@ names_quantity = len(names) - 1
 the_chosen_one_number = random.randint(0, names_quantity)
 the_chosen_one_name = names[the_chosen_one_number]
 print(f"{the_chosen_one_name} is going to buy the meal today!")
+
+#Easier solution
+#import random
+#names_string = input("Give me everybody's names, separated by a comma. ")
+#names = names_string.split(", ")
+#person_who_will_pay = random.choice(names)
+#print(person_who_will_pay + " is going to buy the meal today!")
