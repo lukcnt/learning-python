5e4ff3bbe5f24c187fab40751db474364db17de6
Lukcnt <cantoni.lucas@gmail.com>
2022-01-04 00:50:32 -0300

    Nested lists practice

---
+++ Day 4/Nested lists.py
@@ -0,0 +1,6 @@
+fruits = ["Strawberries", "Nectarines", "Apples", "Grapes", "Peaches", "Cherries", "Pears"]
+vegetables = ["Spinach", "Kale", "Tomatoes", "Celery", "Portatoes"]
+
+dirty_dozen = [fruits, vegetables]
+
+print(dirty_dozen)
