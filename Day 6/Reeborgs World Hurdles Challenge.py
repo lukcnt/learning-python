dcd178c174af10f7dbc2679c5e8e66877cf4e90a
Lukcnt <cantoni.lucas@gmail.com>
2022-01-07 00:26:27 -0300

    Reeborgs World Hurdle Challenge solved

---
+++ Day 6/Reeborgs World Hurdles Challenge.py
@@ -0,0 +1,20 @@
+def turn_around():
+    turn_left()
+    turn_left()
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+for j in range(0,6):
+    move()
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_left()
+
+#https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%201&url=worlds%2Ftutorial_en%2Fhurdle1.json
