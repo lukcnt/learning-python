1426f8354322be20787b8c631c9b3f8c45d0ffa4
Lukcnt <cantoni.lucas@gmail.com>
2022-01-10 14:21:46 -0300

    Paint Area Calculator

---
+++ Day 8/Functions positional and keyword arguments.py
@@ -0,0 +1,23 @@
+#Function
+#def greet(name):
+#    print(f"Hello {name}")
+#    print(f"How do you do {name}?")
+#   print("Isn't the weather nice today?")
+
+#greet("Lucas")
+
+#Functions with more than 1 input
+def greet_with(name, location):
+    print(f"Hello {name}\nWhat is it like in {location}?")
+
+#question = input("Write your name: ")
+#question2 = input("Write your location: ")
+
+#Functions with positional arguments
+#greet_with(question, question2)
+
+#Functions with keyword arguments
+greet_with(location="Serra Leoa", name="Izabel")
+
+#Parameter is the name of the data
+#Argument is the data
