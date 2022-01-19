d19e6f034ccbfb420aa6ce533f13b058ec908aaf
Lukcnt <cantoni.lucas@gmail.com>
2022-01-19 15:05:32 -0300

    Add function for draw a card from the cards list

--- Day 011 - Capstone Project/Day 011 - The Blackjack Program.py
+++ Day 011 - Capstone Project/Day 011 - The Blackjack Program.py
@@ -9,12 +9,21 @@
 ## The cards in the list have equal probability of being drawn.
 ## Cards are not removed from the deck as they are drawn.
 ## The computer is the dealer.
+
 import art
+import random
 
-cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10}]
+cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 
+def draw():
+    card = random.choice(cards)
+    return card
+
+def blackjack():
+    
 start = input("Do you want to play a game of Blackjack? Type 'yes' or 'no': ")
 if start == "yes":
     print(art.logo)
+    blackjack()
 else:
     exit()
