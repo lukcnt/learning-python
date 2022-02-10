8afbaf3dee13b9819ad18e44e14c26b3508032af
lukcnt <lukcnt@outlook.com>
2022-02-10 05:47:25 -0800

    Fixing a bug in the odd or even code

---
+++ Day 013/Exercise 01 - Debugging Odd or Even/main.py
@@ -0,0 +1,15 @@
+# #Code with a bug
+# number = int(input("Which number do you want to check?"))
+
+# if number % 2 = 0:
+#   print("This is an even number.")
+# else:
+#   print("This is an odd number.")
+
+#Bug fixed
+number = int(input("Which number do you want to check?"))
+
+if number % 2 == 0:
+    print("This is an even number.")
+else:
+    print("This is an odd number.")
