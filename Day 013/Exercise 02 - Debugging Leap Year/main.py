29fe4e1d429cea1e12b61cde923fb5d391af9813
lukcnt <lukcnt@outlook.com>
2022-02-10 06:01:33 -0800

    Fixing a bug in the leap year code

---
+++ Day 013/Exercise 02 - Debugging Leap Year/main.py
@@ -0,0 +1,27 @@
+# #Code with a bug
+# year = input("Which year do you want to check?")
+
+# if year % 4 == 0:
+#   if year % 100 == 0:
+#     if year % 400 == 0:
+#       print("Leap year.")
+#     else:
+#       print("Not leap year.")
+#   else:
+#     print("Leap year.")
+# else:
+#   print("Not leap year.")
+
+#Bug fixed
+year = int(input("Which year do you want to check?"))
+
+if year % 4 == 0:
+  if year % 100 == 0:
+    if year % 400 == 0:
+      print("Leap year.")
+    else:
+      print("Not leap year.")
+  else:
+    print("Leap year.")
+else:
+  print("Not leap year.")
