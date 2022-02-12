26449b00f9c2d79e9b6287f472f8f9da509f289f
lukcnt <lukcnt@outlook.com>
2022-02-12 07:08:32 -0800

    high lower game project finished

--- Day 014/main.py
+++ Day 014/main.py
@@ -1,44 +1,45 @@
-#Import the art from the art.py file
 import art
+import game_data
+import random
+import os
 
 #Shows the logo at the beginning of the game
 print(art.logo)
 
-# Import the data from game_data.py file
-import game_data
-import random
+#Conditional for keep the game
+answer_is_right = True
 
-#Randomly chooses two accounts from the data
+#Keep the score of the player
+score = 0
+
+#Randomly chooses an accounts from the data
 first_account = random.choice(game_data.data)
-second_account = random.choice(game_data.data)
 
-#Gives some information about the accounts
-print(f"Compare A: {first_account['name']}, a {first_account['description']}, from {first_account['country']}.")
-print(art.vs)
-print(f"Against B: {second_account['name']}, a {second_account['description']}, from {second_account['country']}.")
+while answer_is_right:
+    second_account = random.choice(game_data.data)
 
-## Loop to go through all the data in the data list(desnecessary)
-# for information in range(len(data)):
-#     print(data[information])
+    #Gives some information about the accounts
+    print(f"Compare A: {first_account['name']}, a {first_account['description']}, from {first_account['country']}.")
+    print(art.vs)
+    print(f"Against B: {second_account['name']}, a {second_account['description']}, from {second_account['country']}.")
 
-## Shows a specific part of the information in the dictionary
-# print(data[0]["name"])
-# print(data[0])
+    #User chooses who they think have more followers
+    user_choice = input("Who has more followers? Type 'A' or 'B': ").lower()
 
-## Visual comparision of the data
-# print(f"Compare A: {data[0]}.")
-# print(vs)
-# print(f"Against B: {data[1]}.")
-
-## Choose a random dictionary in the data list
-# a = random.choice(data)
-# print(a)
-# print(type(a))
-
-## Compare if the data choosen is the same
-# a = game_data.data[0]
-# b = game_data.data[0]
-# if a == b:
-#     print("True")
-# else:
-#     print("False")
+    #Compare the two accounts to see if the player is right and add score
+    if user_choice == "a" and first_account["follower_count"] > second_account["follower_count"]:
+        score += 1
+        os.system('cls')
+        print(art.logo)
+        print(f"You're right! Current score: {score}")
+    elif user_choice == "b" and second_account["follower_count"] > first_account["follower_count"]:
+        score += 1
+        first_account = second_account
+        os.system('cls')
+        print(art.logo)
+        print(f"You're right! Current score: {score} ")
+    else:
+        os.system('cls')
+        print(art.logo)
+        print(f"Sorry, that's wrong. Final score: {score}")
+        answer_is_right = False
