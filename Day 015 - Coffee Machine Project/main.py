cc14ba5dd946ad34e598d88d8a0a5b62b92a09bd
lukcnt <lukcnt@outlook.com>
2022-02-15 09:03:49 -0800

    add the function of report that shows all the resources the coffee machine have

--- Day 015 - Coffee Machine Project/main.py
+++ Day 015 - Coffee Machine Project/main.py
@@ -1,3 +1,16 @@
+import data
+
+
+def report():
+    """ Shows how much of the ingredients and money the coffee machine have"""
+    for supplies in data.resources:
+        if supplies == "water" or supplies == "milk":
+            print(f"{supplies.title()}: {data.resources[supplies]}ml")
+        elif supplies == "coffee":
+            print(f"{supplies.title()}: {data.resources[supplies]}g")
+        else:
+            print(f"{supplies.title()}: ${data.resources[supplies]}")
+
 
 # Keep attending after finishing an order
 next_customer = True
@@ -8,6 +21,15 @@ while next_customer:
 
     if order == "espresso" or order == "latte" or order == "cappuccino":
         print(order)
+
+    # Turn the coffee machine off
     elif order == "off":
         print("The coffee machine will be turned off.")
         next_customer = False
+
+    # Print the resources
+    elif order == "report":
+        report()
+
+    else:
+        print("This isn't a option, please select a option from the menu.")
