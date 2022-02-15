a9071ad2d98853692e95e0c4a45cb18a41dc2338
lukcnt <lukcnt@outlook.com>
2022-02-15 05:24:21 -0800

    loop to keep attending customers

--- Day 015 - Coffee Machine Project/main.py
+++ Day 015 - Coffee Machine Project/main.py
@@ -1,2 +1,6 @@
-# Gives the options for the customer order
-print("What would you like? (espresso/latte/cappuccino): ")
+# Keep attending after finishing an order
+next_customer = True
+
+while next_customer:
+    # Gives the options for the customer order
+    print("What would you like? (espresso/latte/cappuccino): ")
