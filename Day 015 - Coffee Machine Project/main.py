5c3252979ef636d0eabd0ac72630a41343f6bd10
lukcnt <lukcnt@outlook.com>
2022-02-15 06:55:04 -0800

    turning the coffee machine off

--- Day 015 - Coffee Machine Project/main.py
+++ Day 015 - Coffee Machine Project/main.py
@@ -1,6 +1,13 @@
+
 # Keep attending after finishing an order
 next_customer = True
 
 while next_customer:
     # Gives the options for the customer order
-    print("What would you like? (espresso/latte/cappuccino): ")
+    order = input("What would you like? (espresso/latte/cappuccino): ").lower()
+
+    if order == "espresso" or order == "latte" or order == "cappuccino":
+        print(order)
+    elif order == "off":
+        print("The coffee machine will be turned off.")
+        next_customer = False
