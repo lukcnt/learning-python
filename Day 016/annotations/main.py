79c5c5fce84436a892452c45bbafaa1dd7ac7615
lukcnt <lukcnt@outlook.com>
2022-02-19 10:58:18 -0800

    annotations OOP turtle graphics

---
+++ Day 016/annotations/main.py
@@ -0,0 +1,26 @@
+# import turtle
+# roberto = turtle.Turtle()
+
+from turtle import Turtle, Screen
+
+roberto = Turtle()
+print(roberto)
+roberto.shape("turtle")
+roberto.color("DarkCyan")
+roberto.forward(100)
+roberto.left(50)
+roberto.forward(100)
+roberto.left(50)
+roberto.forward(100)
+roberto.left(50)
+roberto.forward(100)
+roberto.left(50)
+roberto.forward(100)
+roberto.left(50)
+roberto.forward(100)
+roberto.left(50)
+roberto.forward(100)
+
+my_screen = Screen()
+print(my_screen.canvheight)
+my_screen.exitonclick()
