75fccaf6045b89d88d267d61a0428c8c84e37f32
lukcnt <lukcnt@outlook.com>
2022-03-30 11:51:37 -0700

    atributes, methods, classes and objects learned for OOP

--- Day 016/annotations/main.py
+++ Day 016/annotations/main.py
@@ -1,12 +1,12 @@
-# import turtle
-# roberto = turtle.Turtle()
+import turtle
+roberto = turtle.Turtle()
 
 from turtle import Turtle, Screen
 
 roberto = Turtle()
 print(roberto)
 roberto.shape("turtle")
-roberto.color("DarkCyan")
+roberto.color("darkcyan")
 roberto.forward(100)
 roberto.left(50)
 roberto.forward(100)
@@ -24,3 +24,11 @@ roberto.forward(100)
 my_screen = Screen()
 print(my_screen.canvheight)
 my_screen.exitonclick()
+
+from prettytable import PrettyTable
+
+table = PrettyTable()
+table.add_column("Pokemon Name", ["Pikachu", "Squirtle", "Charmander"])
+table.add_column("Type", ["Electric", "Water", "Fire"])
+table.align = "l"
+print(table)
